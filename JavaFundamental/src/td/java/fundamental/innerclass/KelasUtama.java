dd8934fced0084b7a8e68dd58d6903dd6a3ac3be
Tedy Darmawan <tedy.darmawan@gmail.com>
2017-05-09 11:50:37 +0700

    ubah nama projek

--- JavaFundamental/src/td/java/fundamental/innerclass/KelasUtama.java
+++
@@ -1,9 +0,0 @@
-package td.java.fundamental.innerclass;
-
-public class KelasUtama {
-	
-	public static void main(String[] args){
-		new Manusia("Tedy Darmawan");
-	}
-	
-}
---
+++ src/src/td/java/fundamental/innerclass/KelasUtama.java
@@ -0,0 +1,9 @@
+package td.java.fundamental.innerclass;
+
+public class KelasUtama {
+	
+	public static void main(String[] args){
+		new Manusia("Tedy Darmawan");
+	}
+	
+}
